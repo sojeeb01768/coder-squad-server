05821c196bc450bf1260794027d239c7941a30c3
sojeeb01768 <sojeebahmed.sa@gmail.com>
2024-04-02 06:29:06 +0600

    post and get email specific  task data

--- index.js
+++ index.js
@@ -26,14 +26,64 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-        
+        const userCollection = client.db('coderSquad').collection('users')
+        const taskCollection = client.db('coderSquad').collection('tasks')
+
+        app.post('/user', async (req, res) => {
+            try {
+                const { name, email } = req.body;
+                // Create a new user document
+                const newUser = {
+                    name,
+                    email,
+                };
+
+                // Insert the user document into the collection
+                await userCollection.insertOne(newUser);
+                res.status(201).json({ message: 'User created successfully' });
+            }
+            catch (error) {
+                console.error(error.message);
+                res.status(500).send('Server Error');
+            }
+        })
+
+        app.post('/addTask', async (req, res) => {
+            const request = req.body;
+            // const task = { ...request }
+            try {
+                const result = await taskCollection.insertOne(request);
+                res.send(result);
+            } catch (error) {
+                console.error(error.message);
+                res.status(500).send('Failed to add task');
+            }
+        });
+
+        app.get('/myTasks/:email', async (req, res) => {
+            try {
+                const result = await taskCollection.find({ email: req.params.email }).toArray();
+                if (!result) {
+                    return res.status(404).send('No tasks found for this email');
+                }
+                console.log(result);
+                res.send(result);
+            } catch (error) {
+                console.error(error.message);
+                res.status(500).send('Failed to get task data');
+            }
+        });
+
+
+
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
